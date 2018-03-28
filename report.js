a3aefae8d262b9238d7df3f8dcfe7b40a83c9f00
cicomsa <ioanacomsa85@gmail.com>
2018-03-28 22:42:47 +0200

    value for animals, yet not the correct value

--- report.js
+++ report.js
@@ -3,4 +3,14 @@ const {Wheat, Sugarcane} = require('./crops')
 const {Cow, Pig, Horse} = require('./animals')
 
 const myFarm = new Farm("FARM")
-console.log(`Farm: ${myFarm.name}`)
+
+myFarm.addAnimal(new Cow(10))
+
+console.log(myFarm.getReport())
+// console.log(`Farm: ${myFarm.name}`)
+// console.log("------------------")
+// console.log(`- Farm: ${myFarm.name} -`)
+// console.log( `- No. of crops: ${myFarm.crops} -`)
+// console.log(`- No. of animals: ${myFarm.animals} -`);
+// console.log(`- Total income: ${myFarm.calculateIncome()} -`);
+// console.log("------------------")
